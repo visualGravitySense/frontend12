32edc7ad8bbea35fb8d43aa925034b430f1a9c78
Dmitri Gornakov <helikeel@gmail.com>
2024-08-16 19:09:43 +0300

    Add files via upload

---
+++ js/part1.js
@@ -0,0 +1,36 @@
+/* In JS everything is object
+"." enter to object
+() call "return" method
+
+Variables use */
+
+// console.log(a);
+console.log("Hello World!");
+
+let a;
+console.log(a);
+a = "Hello World"
+console.log(a);
+a = "Dmitri"
+console.log(a);
+
+let b=10,c=5;
+console.log(b,c);
+
+const d = 666;
+console.log(d);
+// d=111; // can't change "const"
+
+// Do not use "var"
+// var x = 10;
+// console.log(x);
+// var x = 11;
+// console.log(x);
+
+let x = 10;
+console.log(typeof x);
+
+x = 123e5;
+console.log(typeof x, x);
+
+console.log(0.1 + 0.2);
