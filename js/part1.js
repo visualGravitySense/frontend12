b460daa7ffff0ee5a9a6ebb187cb9a69b8d0351d
Dmitri Gornakov <helikeel@gmail.com>
2024-08-16 20:42:45 +0300

    Add files via upload

--- js/part1.js
+++ js/part1.js
@@ -34,3 +34,45 @@ x = 123e5;
 console.log(typeof x, x);
 
 console.log(0.1 + 0.2);
+
+let userName = 'Terminator';
+console.log(typeof userName);
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+console.log("That's");
+console.log("That\\\'s".length);
+// /' escape symbol, not use as a system 
+
+console.log("Hello" + ' ' + userName + (12345 + 123456));
+console.log(userName * 123456);
+
+// \n string return
+// \t tabulation
+// \b backspace
+// console.log(123 /n 456 );
+console.log("\tstring\nhfgtr");
+
+let num = 1;
+console.log(Boolean(num));
+
+let num2 = 0;
+console.log(Boolean(num2));
+
+
+let str = 'Nan';
+console.log(Boolean(str));
+let str2 = '';
+console.log(Boolean(str2));
+
+let str3 = '\n';
+console.log(Boolean(str3));
+
+let car = 'Subaru WRX asd';
+console.log(car, car.length);
+console.log(car[0]);
+console.log(car[car.length - 1]);
+
+// last symbol not iclude to diapason 
+console.log(car.slice(0,5));
+console.log(car.slice(1,3));
